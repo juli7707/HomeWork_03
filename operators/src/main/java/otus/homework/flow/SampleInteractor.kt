1443f60492157f66c62470202bf6a23aba7e149f
j.kardanova <j.kardanova@soax.com>
2021-07-25 16:12:29 +0300

    Implement functions using flow statements

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it*5 }
+            .filter { it > 20 }
+            .filterNot { it%2 == 0 }
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -29,7 +34,15 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .transform {
+                emit(it.toString())
+                when {
+                    it%15 == 0 -> emit("FizzBuzz")
+                    it%5 == 0 -> emit("Buzz")
+                    it%3 == 0 -> emit("Fizz")
+                }
+            }
     }
 
     /**
@@ -38,7 +51,10 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { f1, f2 ->
+                Pair(f1, f2)
+            }
     }
 
     /**
@@ -48,6 +64,14 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { exception ->
+                sampleRepository.completed()
+                when (exception) {
+                    is IllegalArgumentException -> emit(-1)
+                    else -> throw exception
+                }
+            }
+
     }
 }
